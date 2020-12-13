2cdc0cbcdbfbb81e39b5da8975c665a7eb6a35e3
Saurav M H <sauravhiremath@gmail.com>
2020-12-13 13:20:15 +0000

    feat: intro content add

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -5,6 +5,23 @@ export default function Landing() {
   return (
     <>
       <h1 className="glowing-text">Noir Night.</h1>
+      <div className="intro">
+        It's 2:30 am when you arrive at the Smith Mansion. Elizabeth, Bill and
+        Doctor Owl are standing around the bed with the dead body of Mr. Smith.
+        Your partner who arrived early on the scene gives you a rundown of the
+        suspects: Elizabeth the wife of the deceased, Bill his butler and Doctor
+        Owl their family doctor for the last 5 years.
+        <br />
+        Autopsy report says Smith was choked to death. However, the only person
+        next to the body when it was found was Elizabeth, who lay there crying.
+        <br />
+        Bill recalls Mr. Smith calling the doctor over as he suffered from a
+        severe chest pain. But Doctor Owl's car arrived at the mansion half an
+        hour earlier than the body was discovered.
+        <br /><br /><br />
+        With only this information to start with, figure out who's responsible
+        for the murder of Mr. Smith in 2 minutes!
+      </div>
       <div className="btns">
         <Link to="/play" class="play">
           Let's Play!
