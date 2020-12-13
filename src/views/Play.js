4a37713d24e62ac38b21f2d10790076687bdd90d
Saurav M H <sauravhiremath@gmail.com>
2020-12-13 11:52:15 +0000

    feat: onSpeak animation added

--- src/views/Play.js
+++ src/views/Play.js
@@ -11,7 +11,36 @@ export default function Play() {
   const [query, setQuery] = useState('');
   const [textQuery, setTextQuery] = useState('');
   const [audioBuffer, setAudioBuffer] = useState(undefined);
+  const [bars, setBars] = useState([]);
   const [response, setResponse] = useState('Lets get started detective!');
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: false })
+      .then((stream) => {
+        if (stream) {
+          const context = new (window.AudioContext ||
+            window.webkitAudioContext)();
+          const source = context.createMediaStreamSource(stream);
+          const analyser = context.createAnalyser();
+          source.connect(analyser);
+
+          function renderFrame() {
+            requestAnimationFrame(renderFrame);
+            const frequencyData = new Float32Array(256);
+            analyser.getFloatTimeDomainData(frequencyData);
+            // console.log(frequencyData);
+            const bars = [];
+            for (let i = 0; i < 54; i++) {
+              const val = Math.abs(frequencyData[i]);
+              bars.push(`${val * 1000}%`);
+            }
+            setBars(bars);
+          }
+          renderFrame();
+        }
+      });
+  }, []);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -30,6 +59,8 @@ export default function Play() {
         bufferSource.connect(context.destination);
         bufferSource.start();
       });
+
+      return () => context.close();
     }
   }, [audioBuffer]);
 
@@ -79,7 +110,12 @@ export default function Play() {
           <img style={{ maxWidth: '40px' }} src={speak} alt="speak icon" />
         </button>
       </div>
-      {/* {listening && <div>Listening... (or some better wave animation) </div>} */}
+
+      {listening && (
+        <div className="voice-coder">
+          <Visualizer bars={bars} />
+        </div>
+      )}
       <div className="blockquote hint-box">
         <h1>Maybe try asking: Sample placeholder hint</h1>
         <h4>
@@ -108,3 +144,7 @@ export default function Play() {
     </div>
   );
 }
+
+const Visualizer = ({ bars }) => {
+  return bars.map((bar, i) => <span id={`${i + 1}`} style={{ height: bar }} />);
+};
