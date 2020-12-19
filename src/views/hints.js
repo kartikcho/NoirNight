c9665b83a8a6dc99a8204db737289b26a4967fe3
Lilykhan <47777673+Lilykhan786@users.noreply.github.com>
2020-12-19 23:53:17 +0530

    Responsive design and Improved image cards. (#4)

--- src/views/hints.js
+++ src/views/hints.js
@@ -1,5 +1,5 @@
 const hints = [
-  'Mr. Blahaj had heart problems. When did the problems first start?',
+  'Mr. Smith had heart problems. When did the problems first start?',
   'Where was everyone when the body was found? ',
   'Why were there no signs of struggle?',
 ];
